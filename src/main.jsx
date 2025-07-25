06e1034d4e99e917cfa455728c71e2baa7396d07
Rajarajeshwaran-2003 <kannarajesh057@gmail.com>
2025-07-25 17:03:07 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { HashRouter } from 'react-router-dom'; // ✅ import HashRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <HashRouter>  {/* ✅ wrap with HashRouter */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
