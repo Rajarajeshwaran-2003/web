aa9b1344f234cc629a5225ac12ef04733bf8e963
Rajarajeshwaran-2003 <kannarajesh057@gmail.com>
2025-07-25 16:49:29 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { HashRouter } from 'react-router-dom'; // ✅ import HashRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>  {/* ✅ wrap with HashRouter */}
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({ // 👈 This must match your GitHub repo name
+
+export default defineConfig({
+
   plugins: [react()],
+
 });
+
+
