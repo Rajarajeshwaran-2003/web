736420726b82abcd980f68156d98f3bb8c153704
Rajarajeshwaran-2003 <kannarajesh057@gmail.com>
2025-07-25 15:47:02 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rajarajeshwaran-webfolio-/', // 👈 This must match your GitHub repo name
+  base: 'https://github.com/Rajarajeshwaran-2003/rajarajeshwaran-webfolio-.git', // 👈 This must match your GitHub repo name
   plugins: [react()],
 });
