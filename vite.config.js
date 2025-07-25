34e4c233435a99ab36db091c9560d6d66e7801ec
Rajarajeshwaran-2003 <kannarajesh057@gmail.com>
2025-07-25 19:37:24 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/web/', // 🔁 use your repo name here
+  base: '/web/', // <- Set this to your GitHub repo name
 })
